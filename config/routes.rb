141b7d4e0dd963b8b94370943229eee05f9bdad1
vagrant <vagrant@localhost.localdomain>
2020-04-07 06:43:44 +0000

    add login routing to static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   get '/about', to: 'static_pages#about'
 
+  get '/login', to: 'static_pages#login'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
