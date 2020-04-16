9dc81a2588840711ac6187cc7ac66349f05bd545
vagrant <vagrant@localhost.localdomain>
2020-04-17 08:56:30 +0900

    modify

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -53,13 +53,9 @@ class PasswordResetsController < ApplicationController
       params.require(:user).permit(:password, :password_confirmation)
     end
 
-    def @user.password_reset_expired?
-      @user.reset_sent_at < 1.hours.ago
-    end
-
     def check_expiration
       if @user.password_reset_expired?
-        flash[:danger] = "パスワード更新の有効期限が過ぎています"
+        flash[:danger] = "Password reset has expired."
         redirect_to new_password_reset_url
       end
     end
