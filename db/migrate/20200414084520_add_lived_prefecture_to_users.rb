12d6156ba54013328f427fb18c2e9b6ea6b9e92f
vagrant <vagrant@localhost.localdomain>
2020-04-16 15:09:25 +0900

    midify migration file

--- db/migrate/20200414084520_add_lived_prefecture_to_users.rb
+++ db/migrate/20200414084520_add_lived_prefecture_to_users.rb
@@ -1,5 +1,5 @@
 class AddLivedPrefectureToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :lived_prefecture, :integer
+    add_column :users, :lived_prefecture, 'integer USING CAST(lived_prefecture AS integer)'
   end
 end
--- db/migrate/20200415062040_change_data_title_to_users.rb
+++ db/migrate/20200415062040_change_data_title_to_users.rb
@@ -1,7 +1,7 @@
 class ChangeDataTitleToUsers < ActiveRecord::Migration[5.1]
   def change
-    change_column :users, :ssbu_experience, :integer
-    change_column :users, :ssbu_skill, :integer
-    change_column :users, :using_character, :integer
+    change_column :users, :ssbu_experience, 'integer USING CAST(ssbu_experience AS integer)'
+    change_column :users, :ssbu_skill, 'integer USING CAST(ssbu_skill AS integer)'
+    change_column :users, :using_character, 'integer USING CAST(using_character AS integer)'
   end
 end
